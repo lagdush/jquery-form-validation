a98967c9a941a386b358dc2d8a0bdf108ffc76f0
Luke_Z <zurawskiu@gmail.com>
2021-10-04 22:16:15 +0200

    feat: added better email validation and city validation.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // import { inputValidation } from './src/inputValidation';
 // import { zipCodeFormat } from './src/zipCodeFormat';
 //
+// check city zip code
+const checkCityZipCode = (arrWithData) => {
+  const citiesMatchZipCodeArr = arrWithData.filter(
+    (address) => address.miejscowosc === $('#city').val()
+  );
+  if (!citiesMatchZipCodeArr.length) {
+    $('#cityCheck')
+      .text('Podane miasto ma inny kod pocztowy')
+      .css('visibility', 'visible');
+  } else {
+    $('#cityCheck')
+      .text('Podane miasto ma inny kod pocztowy')
+      .css('visibility', 'hidden');
+  }
+};
+//
 const phoneNumberCheck = (phone) => {
   phone.match(/(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/)
     ? $('#phoneCheck').css('visibility', 'hidden')
-    : $('#phoneCheck').css('visibility', 'visible');
+    : $('#phoneCheck')
+        .text('Wpisz poprawny numer telefonu')
+        .css('visibility', 'visible');
+};
+
+const emailCheck = (email) => {
+  email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/)
+    ? $('#emailCheck').css('visibility', 'hidden')
+    : $('#emailCheck')
+        .text('Wpisz poprawny email')
+        .css('visibility', 'visible');
 };
 
 const errorMessageHandler = (inputId, messageId) => {
@@ -23,16 +49,26 @@ export const inputValidation = () => {
   $('#firstname').change(() =>
     errorMessageHandler('#firstname', '#firstnameCheck')
   );
-
   $('#lastname').change(() =>
     errorMessageHandler('#lastname', '#lastnameCheck')
   );
-  $('#email').change(() => errorMessageHandler('#email', '#emailCheck'));
+  $('#email').change(() => {
+    errorMessageHandler('#email', '#emailCheck');
+    emailCheck($('#email').val());
+  });
   $('#phone').change(() => phoneNumberCheck($('#phone').val()));
 
   $('#zip').change(() => zipCodeCheck($('#zip').val()));
 
-  $('#city').change(() => errorMessageHandler('#city', '#cityCheck'));
+  $('#city').change(() => {
+    errorMessageHandler('#city', '#cityCheck');
+
+    const rawDataFromStorage = localStorage.getItem('data');
+    if (!rawDataFromStorage) {
+      return;
+    }
+    checkCityZipCode(JSON.parse(rawDataFromStorage));
+  });
 
   $('#street').change(() => errorMessageHandler('#street', '#streetCheck'));
   $('#house-number').change(() =>
@@ -57,8 +93,6 @@ $(document).ready(function () {
   const dropdownCity = $('#city-list');
   const dropdownStreet = $('#street-list');
 
-  inputValidation();
-
   //API calls
   function createCityAndStreetLists() {
     dropdownCity.empty();
@@ -72,10 +106,12 @@ $(document).ready(function () {
     if (!correctZipCode) return;
     const url = `http://kodpocztowy.intami.pl/api/${correctZipCode}`;
     $.getJSON(url, function (data) {
+      localStorage.setItem('data', JSON.stringify(data));
       const citiesMatchZipCodeArr = data.filter(
         (address) => address.miejscowosc === $('#city').val()
       );
-      if (!citiesMatchZipCodeArr.length) {
+
+      if ($('#city').val() !== '' && !citiesMatchZipCodeArr.length) {
         $('#cityCheck')
           .text('Podane miasto ma inny kod pocztowy')
           .css('visibility', 'visible');
@@ -108,6 +144,8 @@ $(document).ready(function () {
       });
     });
   }
+
+  inputValidation();
 
   $('#zip').change(createCityAndStreetLists);
 });
